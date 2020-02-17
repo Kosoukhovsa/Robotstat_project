403c9e102b77b71f65d26806d63eca4ca41f3c18
Serg <kosoukhovsa@gmail.com>
2020-02-17 16:40:24 +0300

    xx

--- Tools/filetools.py
+++ Tools/filetools.py
@@ -8,31 +8,27 @@ class FileEngine():
     # Возвращает содержимое листа Excel в виде словаря строк
     def GetListData(listname):
         path_name = os.path.join(basedir, 'Tools/Dictionaries.xlsx')
-
-        #try:
-        f = pd.read_excel(path_name, listname)
-
-        #except:
-        #    return {}
-        #else:
-        d = f.to_dict(orient='records')
-        return d
+        try:
+            f = pd.read_excel(path_name, listname)
+        except:
+            return {}
+        else:
+            d = f.to_dict(orient='records')
+            return d
 
     @staticmethod
     # Возвращает список справочников
     def GetDictList():
         path_name = os.path.join(basedir, 'Tools\Dictionaries.xlsx')
-
-        try:
-            f = pd.read_excel(path_name, 'Dict_list')
-            print(f)
-        except:
-            return {}
-        else:
-            d = f.to_dict(orient='records')
-            #d = sorted(d.items(), key=lambda x:x[1])
-            dict_list = []
-            for i in d:
-                dict_list.append(i['Dictionary'])
-
-            return dict_list
+        #try:
+        f = pd.read_excel(path_name, 'Dict_list')
+        print(f)
+        #except:
+        #return {}
+        #else:
+        d = f.to_dict(orient='records')
+        #d = sorted(d.items(), key=lambda x:x[1])
+        dict_list = []
+        for i in d:
+            dict_list.append(i['Dictionary'])
+        return dict_list
